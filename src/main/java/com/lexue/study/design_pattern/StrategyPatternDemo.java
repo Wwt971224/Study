de001d69856a045e4de1553c8bd2c6cc34d655f9
wentai <wangwentai@lexue.com>
2021-06-26 18:57:06 +0800

    测试合并冲突master代码

--- src/main/java/com/lexue/study/design_pattern/StrategyPatternDemo.java
+++ src/main/java/com/lexue/study/design_pattern/StrategyPatternDemo.java
@@ -43,6 +43,7 @@ public class StrategyPatternDemo {
     public static void main(String[] args) {
         System.out.println("1");
         System.out.println("2");
+        System.out.println("3");
         StrategyContext strategyContext;
 
         strategyContext = new StrategyContext(new ConcreteStrategyA());
