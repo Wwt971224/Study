57f51ac6d03d95bf93ed0cd34f226ccba78932d0
wtai <1490999249@qq.com>
2021-11-10 22:04:20 +0800

    随便写的测试

--- src/main/java/com/lexue/study/other/HelloWorld.java
+++ src/main/java/com/lexue/study/other/HelloWorld.java
@@ -6,5 +6,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!!!");
         HashMap<String, String> map = new HashMap<>();
+        map.put("1", "!");
     }
 }
