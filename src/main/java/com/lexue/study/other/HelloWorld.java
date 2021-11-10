50a7335e46504dd80e0aa5e721b964dbae1bf99c
wtai <1490999249@qq.com>
2021-11-10 21:57:47 +0800

    这是个测试

--- src/main/java/com/lexue/study/other/HelloWorld.java
+++ src/main/java/com/lexue/study/other/HelloWorld.java
@@ -1,7 +1,10 @@
 package com.lexue.study.other;
 
+import java.util.HashMap;
+
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!!!");
+        HashMap<String, String> map = new HashMap<>();
     }
 }
